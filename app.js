8cb983788022a7e9fd72a75212428efd63be6a8c
nardos322 <nahuel.rlz@gmail.com>
2022-03-15 23:21:42 -0300

    addhome

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-let suma =  (a, b) => a + b;
-let restar = (b, c) => b - c ;
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes 
+
+app.get('/', (req, res) => {
+    let home = path.join(__dirname, '/views/index.html');
+    res.sendFile(home);
+});
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
