403620f6613fcb28b34ac7037f0362a761141e8e
Nico Dumon <nico.dumon1@gmail.com>
2022-04-25 02:46:20 -0300

    agregando estilos a login y carrito, tambien agregando rutas para users.json

--- src/data/index.js
+++ src/data/index.js
@@ -10,4 +10,8 @@ module.exports = {
     writeAccesories: (data) => {
         fs.writeFileSync(path.join(__dirname, '/accesories.json'), JSON.stringify(data));
     },
+    usuarios: JSON.parse(fs.readFileSync(path.join(__dirname, '/users.json'), 'utf-8')),
+    writeUsers: (data) => {
+        fs.writeFileSync(path.join(__dirname, '/users.json'), JSON.stringify(data));
+    },
 }
