5d4c24fea3d6f4fa7eca01f30cb8e53d20400ab0
nardos322 <nahuel.rlz@gmail.com>
2022-04-25 07:27:33 -0300

    inicio CRUD products

--- src/controllers/admin/adminProductsController.js
+++ src/controllers/admin/adminProductsController.js
@@ -1,7 +1,33 @@
+const {guitars, accesories, writeGuitars, writeAccesories} = require('../../data');
 
 
+const adminProductsController = {
+    
+    //muestra listado de productos
+    list: (req, res) => {
+        let products = [...guitars, ...accesories];
 
+        res.render('admin/products/listProducts', {
+            products,
+        })
+    },
+    // muestra detalle del producto en admin
+    detail: (req, res) => {
+        let products = [...guitars, ...accesories];
+        let productId = +req.params.id;
+        let product = products.find(product => product.id == productId);
 
+        
+        res.render('admin/products/adminDetail', {
+            product,
+            products,
+        })
+    },
+
+    add: (req, res) => {
+        res.render('admin/products/addProduct');
+    }
+}
 
 
 
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const adminProductsController = require('../controllers/admin/adminProductsController');
 
+
+//CRUD de productos
+
+//GET - Lista de productos 
+router.get('/products', adminProductsController.list);
+//GET - detalle de producto vista admin
+router.get('/product/detail/:id', adminProductsController.detail);
+//GET envia vista de formulario para agregar un producto
+router.get('/products/add', adminProductsController.add);
 
 
 
